320de36fad181abe91cd6a4d5258f86d7fafa0e6
cw <fromnowwon@gmail.com>
2023-08-18 20:59:31 +0900

    handling query error

--- src/components/pages/RQSuperHeroes.js
+++ src/components/pages/RQSuperHeroes.js
@@ -1,13 +1,19 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
+const fetchSuperHeroes = () => {
+	return axios.get('http://localhost:4000/superheroes');
+}
+
 const RQSuperHeroesPage = () => {
-	const { isLoading, data } = useQuery('super-heroes', () => {
-		return axios.get('http://localhost:4000/superheroes');
-	});
+	const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes);
 
 	if (isLoading) {
 		return <h2>Loading....</h2>
+	}
+
+	if (isError) {
+		return <h2>{error.message}</h2>
 	}
 
 	return (
--- src/components/pages/SuperHeroes.js
+++ src/components/pages/SuperHeroes.js
@@ -2,19 +2,27 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 const SuperHeroesPage = () => {
-	const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+	const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+	const [error, setError] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:4000/superheroes').then(res => {
       setData(res.data)
       setIsLoading(false)
-    })
+    }).catch(error => {
+			setError(error.message);
+			setIsLoading(false);
+		})
   }, [])
 
   if (isLoading) {
     return <h2>Loading...</h2>
   }
+
+	if (error) {
+		return <h2>{error}</h2>
+	}
 
   return (
     <>
